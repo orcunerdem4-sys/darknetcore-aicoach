51289b9155d1d11a3d676f707015d81ae74a1319
Beytullah <test@example.com>
2026-03-06 09:09:10 +0300

    Update links and layout, fix registration redirect, prepare for deployment

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<DatabaseService>();
