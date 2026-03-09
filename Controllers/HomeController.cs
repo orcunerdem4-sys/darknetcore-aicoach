7583d3489cdbc5666ffa2c23c1d7798950ef0e95
Beytullah <test@example.com>
2026-03-10 01:44:55 +0300

    Add exception details to Error page for debugging

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,10 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
+        var exceptionHandlerPathFeature = HttpContext.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerPathFeature>();
+        ViewData["ExceptionMessage"] = exceptionHandlerPathFeature?.Error?.Message;
+        ViewData["ExceptionStackTrace"] = exceptionHandlerPathFeature?.Error?.StackTrace;
+        
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
